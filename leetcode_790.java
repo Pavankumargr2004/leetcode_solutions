fcd7ab6647696c854fe5c128d8d2cb93d5430713
Pavankumargr2004 <parun4325@gmail.com>
2025-05-07 16:27:31 +0530

    Add files via upload
    leetcode_790

---
+++ leetcode_790.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class leetcode_790 {
+
+    public static int numTilings(int n) {
+        final int MOD = 1_000_000_007;
+        if (n == 0) return 1;
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+
+        long[] dp = new long[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (2 * dp[i - 1] + dp[i - 3]) % MOD;
+        }
+
+        return (int) dp[n];
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = 3;
+
+        int result = numTilings(n);
+        System.out.println("Number of ways to tile a 2 x " + n + " board: " + result);
+    }
+}
+
