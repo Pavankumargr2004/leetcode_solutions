2f4cca2290674b31208bf48f810b01432a646017
Pavankumargr2004 <parun4325@gmail.com>
2025-05-08 14:07:25 +0530

    Add files via upload

---
+++ leetcode_1920.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class leetcode_1920 {
+    public int[] buildArray(int[] nums) {
+        int[] ans = new int[nums.length];
+        
+        for (int i = 0; i < nums.length; i++) {
+            ans[i] = nums[nums[i]];
+        }
+
+        return ans;
+    }
+}
+
